a152980ef4b23e94dedc1306aa332397e3dec761
Mateo Beltran <danielbelnuz18@gmail.com>
2023-11-21 12:01:45 -0500

    clean code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-module.exports = {
-  entities: require('./entities'),
-  models: require('./models'),
-  providers: require('./providers'),
-  repositories: require('./repositories'),
-};
+export * from './entities';
+export * from './models';
+export * from './providers';
+export * from './repositories';
---
+++ src/repositories/index.ts
@@ -0,0 +1,6 @@
+export * from './AccountRepository';
+export * from './AccountingSourceRepository';
+export * from './AccountingVoucherRecordRepository';
+export * from './AccountingVoucherRepository';
+export * from './ItemAccountRepository';
+export * from './ThirdyParthyRepository';
